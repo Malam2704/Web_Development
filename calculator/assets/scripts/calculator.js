95f012695c9f647029547db6313108e9b03c738e
Mohammed Alam <ma3655@rit.edu>
2021-06-26 20:17:24 -0400

    Calculator componenet created

--- calculator/assets/scripts/calculator.js
+++ calculator/assets/scripts/calculator.js
@@ -1,11 +1,48 @@
-var equation;
+var currentNum = 0;
+var firstNum = 0;
+var secondNum = 0;
+var currentOperand;
+
+function clear(){
+    document.getElementById("screen").innerHTML = "";
+}
 
 function enterNum(value){
     var textSoFar = document.getElementById("screen").innerHTML;
     document.getElementById("screen").innerHTML = textSoFar + value;
-    equation = textSoFar + value;
+    currentNum = textSoFar + value;
+}
+
+function addNum(){
+    firstNum = document.getElementById("screen").innerHTML;
+    document.getElementById("screen").innerHTML = "";
+    currentOperand = "+";
+}
+function subNum(){
+    firstNum = document.getElementById("screen").innerHTML;
+    document.getElementById("screen").innerHTML = "";
+    currentOperand = "-";
+}
+function mulNum(){
+    firstNum = document.getElementById("screen").innerHTML;
+    document.getElementById("screen").innerHTML = "";
+    currentOperand = "*";
+}
+function divNum(){
+    firstNum = document.getElementById("screen").innerHTML;
+    document.getElementById("screen").innerHTML = "";
+    currentOperand = "/";
 }
 
 function calcEnter(){
-    alert(equation);
+    secondNum = document.getElementById("screen").innerHTML;
+    if(currentOperand == "+"){
+        document.getElementById("screen").innerHTML = parseInt(firstNum) + parseInt(secondNum);
+    }else if(currentOperand == "-"){
+        document.getElementById("screen").innerHTML = parseInt(firstNum) - parseInt(secondNum);
+    }else if(currentOperand == "*"){
+        document.getElementById("screen").innerHTML = parseInt(firstNum) * parseInt(secondNum);
+    }else if(currentOperand == "/"){
+        document.getElementById("screen").innerHTML = parseInt(firstNum) / parseInt(secondNum);
+    }
 }
