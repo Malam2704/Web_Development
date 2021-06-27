31d7069dfb5169bd1173f6383c7dd19157aa062d
Mohammed Alam <ma3655@rit.edu>
2021-06-26 20:54:34 -0400

    Made calc

--- calculator/assets/scripts/calculator.js
+++ calculator/assets/scripts/calculator.js
@@ -8,65 +8,65 @@ var currentOperand;
 function clearCalc(){
     currentNum = 0;
     firstNum = 0;
-    document.getElementById("screen").innerHTML = "";
+    document.getElementById("numbersArea").innerHTML = "";
 }
 
 // Inputting severl numbers on the calc
 function enterNum(value){
-    var textSoFar = document.getElementById("screen").innerHTML;
-    document.getElementById("screen").innerHTML = textSoFar + value;
+    var textSoFar = document.getElementById("numbersArea").innerHTML;
+    document.getElementById("numbersArea").innerHTML = textSoFar + value;
     currentNum = textSoFar + value;
 }
 
 // One function for each operand, sets firNum var to whatever is on
-// the screen, then clears screen, settignt he way for the next operand
+// the numbersArea, then clears numbersArea, settignt he way for the next operand
 // and sets the currentOperand to +,-,*, or /
 function addNum(){
-    firstNum = document.getElementById("screen").innerHTML;
-    document.getElementById("screen").innerHTML = "";
+    firstNum = document.getElementById("numbersArea").innerHTML;
+    document.getElementById("numbersArea").innerHTML = "";
     currentOperand = "+";
 }
 function subNum(){
-    firstNum = document.getElementById("screen").innerHTML;
-    document.getElementById("screen").innerHTML = "";
+    firstNum = document.getElementById("numbersArea").innerHTML;
+    document.getElementById("numbersArea").innerHTML = "";
     currentOperand = "-";
 }
 function mulNum(){
-    firstNum = document.getElementById("screen").innerHTML;
-    document.getElementById("screen").innerHTML = "";
+    firstNum = document.getElementById("numbersArea").innerHTML;
+    document.getElementById("numbersArea").innerHTML = "";
     currentOperand = "*";
 }
 function divNum(){
-    firstNum = document.getElementById("screen").innerHTML;
-    document.getElementById("screen").innerHTML = "";
+    firstNum = document.getElementById("numbersArea").innerHTML;
+    document.getElementById("numbersArea").innerHTML = "";
     currentOperand = "/";
 }
 function powNum(){
-    firstNum = document.getElementById("screen").innerHTML;
-    document.getElementById("screen").innerHTML = "";
+    firstNum = document.getElementById("numbersArea").innerHTML;
+    document.getElementById("numbersArea").innerHTML = "";
     currentOperand = "^";
 }
 function modNum(){
-    firstNum = document.getElementById("screen").innerHTML;
-    document.getElementById("screen").innerHTML = "";
+    firstNum = document.getElementById("numbersArea").innerHTML;
+    document.getElementById("numbersArea").innerHTML = "";
     currentOperand = "MOD";
 }
 
 // When the enter key is pressed, checks what the current operand is,
-// and sets the screen innerHTML to whatever the equation is.
+// and sets the numbersArea innerHTML to whatever the equation is.
 function calcEnter(){
-    secondNum = document.getElementById("screen").innerHTML;
+    secondNum = document.getElementById("numbersArea").innerHTML;
     if(currentOperand == "+"){
-        document.getElementById("screen").innerHTML = parseFloat(firstNum) + parseFloat(secondNum);
+        document.getElementById("numbersArea").innerHTML = parseFloat(firstNum) + parseFloat(secondNum);
     }else if(currentOperand == "-"){
-        document.getElementById("screen").innerHTML = parseFloat(firstNum) - parseFloat(secondNum);
+        document.getElementById("numbersArea").innerHTML = parseFloat(firstNum) - parseFloat(secondNum);
     }else if(currentOperand == "*"){
-        document.getElementById("screen").innerHTML = parseFloat(firstNum) * parseFloat(secondNum);
+        document.getElementById("numbersArea").innerHTML = parseFloat(firstNum) * parseFloat(secondNum);
     }else if(currentOperand == "/"){
-        document.getElementById("screen").innerHTML = parseFloat(firstNum) / parseFloat(secondNum);
+        document.getElementById("numbersArea").innerHTML = parseFloat(firstNum) / parseFloat(secondNum);
     }else if(currentOperand == "^"){
-        document.getElementById("screen").innerHTML = parseFloat(firstNum) ** parseFloat(secondNum);
+        document.getElementById("numbersArea").innerHTML = parseFloat(firstNum) ** parseFloat(secondNum);
     }else if(currentOperand == "MOD"){
-        document.getElementById("screen").innerHTML = parseFloat(firstNum) % parseFloat(secondNum);
+        document.getElementById("numbersArea").innerHTML = parseFloat(firstNum) % parseFloat(secondNum);
     }
 }
